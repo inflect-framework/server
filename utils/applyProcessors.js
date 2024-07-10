7ee754e59fd8d5cbe519ca53bc6d32f9300ed217
benhancock <benhancock859@gmail.com>
2024-07-10 13:40:12 -0400

    fix: fixed new pipeline and test event routes and logic
    - fixed ability to insert client-authored pipeline into DB
    - fixed ability to pull schema from kafka schema registry
    - fixed ability to generate test event against pulled schema
    - fixed ability to test event against pipeline processors
    
    Co-authored-by: Chris Douglass <cedouglass@gmail.com>

--- utils/applyProcessors.js
+++ utils/applyProcessors.js
@@ -1,18 +1,21 @@
 const db = require('../db');
 
-const getProcessorName = async (processorId) => {
-  const result = await db.query('SELECT processor_name FROM processors WHERE id = $1', [processorId]);
-  if (result.rowCount === 0) {
-    throw new Error(`Processor with id ${processorId} not found`);
-  }
-  return result.rows[0].processor_name;
-};
+// const getProcessorName = async (processorId) => {
+//   const result = await db.query('SELECT processor_name FROM processors WHERE id = $1', [processorId]);
+//   if (result.rowCount === 0) {
+//     throw new Error(`Processor with id ${processorId} not found`);
+//   }
+//   return result.rows[0].processor_name;
+// };
 
-const applyTransformations = async (message, steps, dlqSteps) => {
+const applyProcessors = async (message, steps, dlqSteps) => {
   let transformedMessage = { ...message };
+  console.log(message)
+  console.log(JSON.stringify(transformedMessage) + '$$$$$$')
 
   for (let i = 0; i < steps.length; i++) {
-    const processorName = await getProcessorName(steps[i]);
+    // const processorName = await getProcessorName(steps[i]);
+    processorName = steps[i]
     const transformation = require(`../../stream-processor/transformations/${processorName}`);
     
     try {
@@ -20,24 +23,21 @@ const applyTransformations = async (message, steps, dlqSteps) => {
       console.log('Ran process ' + processorName);
       console.log(transformedMessage);
 
-      if (!transformedMessage) {
-        if (dlqSteps && dlqSteps[i]) {
-          const dlqTopicName = await getDlqTopicName(dlqSteps[i]);
-          return { dlqMessage: message, dlqTopicName };
-        } else {
-          return { transformedMessage: null, filteredAt: processorName };
-        }
-      }
+      // if (!transformedMessage) {
+      //   if (dlqSteps && dlqSteps[i]) {
+      //     const dlqTopicName = await getDlqTopicName(dlqSteps[i]);
+      //     return { dlqMessage: message, dlqTopicName };
+      //   } else {
+      //     return { transformedMessage: null, filteredAt: processorName };
+      //   }
+      // }
     } catch (error) {
       console.error(`Error applying transformation ${processorName}:`, error);
       return { transformedMessage: null, filteredAt: processorName, error: error.message };
     }
   }
 
-  return { transformedMessage };
+  return transformedMessage;
 };
 
-module.exports = {
-  applyTransformations,
-  getProcessorName,
-};
+module.exports = applyProcessors
